2e57438df3ea5563b85f688a62e66830d66ad12c
Smorodin I.T <smorodin.it@gmail.com>
2022-04-10 13:17:32 +0300

    TextFieldDebounced component

--- src/components/inputs/TextFieldDebounced/TextFieldDebounced.tsx
+++ src/components/inputs/TextFieldDebounced/TextFieldDebounced.tsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { ChangeEvent, useState } from 'react';
+import useDebounce from '../../../utils/hooks/useDebounce';
+import { TextField, TextFieldProps } from '@mui/material';
 
-interface TextFieldDebouncedProps {
-  a?: any;
-}
+type TextFieldDebounceEvent = ChangeEvent<
+  HTMLInputElement | HTMLTextAreaElement
+>;
+
+type TextFieldDebouncedProps = {
+  value: string;
+  onChange: (event: TextFieldDebounceEvent) => void;
+  debounceDelay?: number;
+} & Omit<TextFieldProps, 'value' | 'onChange'>;
 
 const TextFieldDebounced = (props: TextFieldDebouncedProps): JSX.Element => {
-  return <div></div>;
+  const [value, setValue] = useState(props.value);
+
+  const handleDebounceOnChange = useDebounce((event) => {
+    if (props.onChange) {
+      props.onChange((event as [TextFieldDebounceEvent])?.[0]);
+    }
+  }, props.debounceDelay ?? 500);
+
+  const handleFieldChange = (event: TextFieldDebounceEvent): void => {
+    setValue(event.target.value);
+    handleDebounceOnChange([event]);
+  };
+
+  return <TextField {...props} value={value} onChange={handleFieldChange} />;
 };
 
 export default TextFieldDebounced;
